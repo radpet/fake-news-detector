69004273e7b932aff0c9b21301aaae8346282744
Radoslav Petkov <radipirate@gmail.com>
2018-05-06 23:14:41 +0300

    write script for predictions

--- stance/mergedata.py
+++ stance/mergedata.py
@@ -1,13 +1,22 @@
 import pandas as pd
 
 
-def load_train():
-    return pd.read_csv('data/train/train_stances.csv'), pd.read_csv('data/train/train_bodies.csv')
-
-
 def merge_train():
-    print('Merging train_stances and train_bodies....')
-    stances, bodies = load_train()
+    stances, bodies = load('data/train/train_stances.csv', 'data/train/train_bodies.csv')
+    return merge(stances, bodies, 'data/train/train.csv')
+
+
+def merge_test():
+    stances, bodies = load('data/test/competition_test_stances.csv', 'data/test/competition_test_bodies.csv')
+    return merge(stances, bodies, 'data/test/test.csv')
+
+
+def load(stances, bodies):
+    return pd.read_csv(stances), pd.read_csv(bodies)
+
+
+def merge(stances, bodies, output):
+    print('Merging {} and {}....'.format(stances, bodies))
 
     merged = pd.merge(stances, bodies, on='Body ID', how='outer')
 
@@ -15,13 +24,14 @@ def merge_train():
     print('....Bodies frame shape', bodies.shape)
     print('....Merged frame shape', merged.shape)
 
-    merged.to_csv('data/train/train.csv', index=False)
+    merged.to_csv(output, index=False)
 
-    print('Merged was successful')
+    print('Merge was successful')
 
 
 def run():
-    merge_train()
+    # merge_train()
+    merge_test()
 
 
 if __name__ == '__main__':
---
+++ stance/predict.py
@@ -0,0 +1,80 @@
+from keras.preprocessing.sequence import pad_sequences
+
+from common.TokenizerSerializer import TokenizerSerializer
+from keras.models import load_model
+import pandas as pd
+from sklearn.metrics import confusion_matrix, classification_report
+from stance.bi_lstm_baseline import MAXLEN_HEADLINE, MAXLEN_BODY
+
+import os
+import numpy as np
+import pickle
+
+
+def load_checkpoint(path):
+    return load_model(path)
+
+
+def load_data(path):
+    return pd.read_csv(path)
+
+
+def get_features(data, tokenizer_headline, tokenizer_body):
+    X = {
+        'headline': data['Headline'],
+        'body': data['articleBody']
+    }
+
+    X['headline'] = tokenizer_headline.texts_to_sequences(X['headline'])
+    X['headline'] = pad_sequences(X['headline'], maxlen=MAXLEN_HEADLINE)
+
+    X['body'] = tokenizer_body.texts_to_sequences(X['body'])
+    X['body'] = pad_sequences(X['body'], maxlen=MAXLEN_BODY)
+
+    return X['headline'], X['body']
+
+
+def get_labels(data, label_to_id):
+    y = data['Stance']
+
+    y = y.map(lambda label: label_to_id[label]).values
+    one_hot = np.zeros((y.shape[0], len(label_to_id)))
+    one_hot[np.arange(y.shape[0]), y] = 1
+
+    return one_hot
+
+
+def run():
+    checkpoint_folder = './checkpoints/2018-04-08_14:42:10'
+    tokenizer_headline = TokenizerSerializer.load(os.path.join(checkpoint_folder, 'tokenizer_headline.pkl'))
+    tokenizer_body = TokenizerSerializer.load(os.path.join(checkpoint_folder, 'tokenizer_body.pkl'))
+
+    with open(os.path.join(checkpoint_folder, 'label_to_id.pkl'), 'rb') as f:
+        label_to_id = pickle.load(f)
+
+    id_to_label = {i: label for label, i in label_to_id.items()}
+
+    data = load_data(path='./data/test/test.csv')
+    X_headline, X_body = get_features(tokenizer_body=tokenizer_body, tokenizer_headline=tokenizer_headline, data=data)
+    y = get_labels(data, label_to_id)
+
+    print(X_headline.shape)
+    print(X_body.shape)
+    model = load_checkpoint(os.path.join(checkpoint_folder, 'weights.17-0.34.hdf5'))
+    model.summary()
+
+    predictions = model.predict([X_headline, X_body])
+
+    df_predictions = data.copy()
+    df_predictions['stance_pred'] = np.array([id_to_label[pred] for pred in np.argmax(predictions, axis=1)]) \
+        .reshape((-1, 1))
+
+    # df_predictions.to_csv(os.path.join(checkpoint_folder, 'test_predicted.csv'), index=False)
+    print('Predicted {}'.format(predictions.shape))
+
+    print(confusion_matrix(y_true=df_predictions['Stance'], y_pred=df_predictions['stance_pred']))
+    print(classification_report(y_true=df_predictions['Stance'], y_pred=df_predictions['stance_pred']))
+
+
+if __name__ == '__main__':
+    run()
